3b6393bf5b3f9619bf5e8d160dbe940871cfa7ad
Thodoris Trifonopoulos <tedtrifonopoulos@gmail.com>
2025-08-11 17:21:04 +0300

    Exploration of order items

--- notebooks/explore_order_items.py
+++ notebooks/explore_order_items.py
@@ -12,5 +12,17 @@ import numpy as np
 print("=" * 60)
 print("💰 ORDER ITEMS DATASET EXPLORATION")
 print("=" * 60)
+
 # Φορτώνουμε το order items dataset
 df = pd.read_csv('/Users/thodoristrifonopoulos/Desktop/projects/ecommerce-dashboard/data/olist_order_items_dataset.csv')
+# Data shape & structure
+print(f"📦 Total number of order items: {df.shape[0]:,}")
+print(f"📋 Number of columns: {df.shape[1]}")
+print("Preview of first 5 rows:")
+print(df.head())
+# Type of data for each column
+print(f"\n🔢 TYPE OF DATA: ")
+print(df.dtypes)
+# Missing values analysis
+print(f"\n⚠️ MISSING VALUES ANALYSIS")
+print(df.isnull().sum())
