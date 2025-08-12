40d7e6b0f7abe138c42bac9d13fc641b87014765
Thodoris Trifonopoulos <tedtrifonopoulos@gmail.com>
2025-08-12 19:42:34 +0300

    Remove test.py from tracking

--- notebooks/test.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-
-# Προσθες αυτό στην αρχή του κώδικα σου (μετά τα imports)
-df= pd.read_csv('/Users/thodoristrifonopoulos/Desktop/projects/ecommerce-dashboard/data/olist_orders_dataset.csv')
-first_col=df['order_id'].iloc[:0]
-print(first_col.head())
-
-
